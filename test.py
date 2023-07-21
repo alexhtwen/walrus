990d739c852ff4689f6ba1a70b7d99a1585990b9
Alex Van <alexhtwen@gmail.com>
2023-07-22 00:28:14 +0800

    minors

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-letters = ['a', 'b', 'c']
-letters = letters + 'xyz'
-print(f'{letters = }')
+from IPython.core.display import HTML
+HTML(r"""
+<style>
+    .output-plaintext, .output-stream, .output {
+        line-height: 1.2 !important;
+        font-size: 40px !important;
+    }
+</style>
+""")
+
+
+print('Hello, world.')
